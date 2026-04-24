b5f71fd63dd207c8634cfcf612c423bb21c8952a
rioH2313 <rhirao2503@g.ucla.edu>
2026-04-24 00:53:43 -0700

    Create WelcomePage component with navigation links
    Added a welcome page with project description and navigation buttons.

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,1 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
+function WelcomePage() {
+  return (
+    <div>
+      <h1> Welcome to CS35L GroupHub </h1>
+      <p>
+        A project management app for CS35L students to communicate, 
+        schedule meetings, and organize group tasks.
+      </p>
+
+      <Link to="/login">
+        <button>Log In</button>
+      </Link>
+
+      <Link to="/signup">
+        <button>Sign Up</button>
+      </Link>
+    </div>
+    );
+}
+export default WelcomePage;
