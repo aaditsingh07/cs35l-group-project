e6db551410d2cdaf4047b5ec6bc472f3d2191d1c
aaditsingh07 <aaditsingh07@gmail.com>
2026-04-24 18:06:10 -0700

    Fix typo in App.js and update dependencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 import WelcomePage from "./pages/WelcomePage";
 import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
+import SignUpPage from "./pages/SignupPage";
 
 function App() {
   return (
