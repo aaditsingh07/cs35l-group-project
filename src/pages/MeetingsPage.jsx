db2728d15f3eb6ca02c8145532ae34dbd439a70d
rioH2313 <rhirao2503@g.ucla.edu>
2026-04-30 22:39:01 -0700

    Update MeetingsPage.jsx
    Created a placeholder MeetingsPage component for future meeting scheduling features. This page will later allow users to propose meeting times and view group availability. Added the initial structure to support future development.

--- src/pages/MeetingsPage.jsx
+++ src/pages/MeetingsPage.jsx
@@ -1,1 +1,10 @@
+function MeetingsPage() {
+  return (
+    <div>
+      <h1>Meetings</h1>
+      <p>This page will allow users to propose and view meeting times.</p>
+    </div>
+  );
+}
 
+export default MeetingsPage;
