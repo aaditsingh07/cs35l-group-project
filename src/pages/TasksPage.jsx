6232bbb093aa3ce85b1bbc4db9a24a9179051ee7
rioH2313 <rhirao2503@g.ucla.edu>
2026-04-30 22:37:28 -0700

    Update TasksPage.jsx
    Created a placeholder TasksPage component for the future task board feature. This page will later allow users to create, manage, and resolve group tasks. Added the initial page structure so routing and future development can be implemented more easily.

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,1 +1,10 @@
+function TasksPage() {
+  return (
+    <div>
+      <h1>Task Board</h1>
+      <p>This page will allow group members to create and manage tasks.</p>
+    </div>
+  );
+}
 
+export default TasksPage;
