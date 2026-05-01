661f850be3fcb93fef9094f2c65176499ce34ca2
rioH2313 <rhirao2503@g.ucla.edu>
2026-04-30 22:38:28 -0700

    Update GroupPage.jsx
    Created a placeholder GroupPage component for future group information and member management features. This page will later display project group members and related group information. Added the initial structure for future implementation.

--- src/pages/GroupPage.jsx
+++ src/pages/GroupPage.jsx
@@ -1,1 +1,10 @@
+function GroupPage() {
+  return (
+    <div>
+      <h1>Your Group</h1>
+      <p>This page will show group members and group information.</p>
+    </div>
+  );
+}
 
+export default GroupPage;
