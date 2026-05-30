74ac45dadaf91be471bfe6a28e524c51d5e5db5e
jasminenaguib <jnaguib2004@g.ucla.edu>
2026-05-30 16:38:53 -0700

    Add Playwright test for meetings route

--- e2e/login.spec.js
+++ e2e/login.spec.js
@@ -5,3 +5,11 @@ test("homepage loads", async ({ page }) => {
 
   await expect(page).toHaveURL(/localhost:3000/);
 });
+
+test("meetings route is reachable", async ({ page }) => {
+    await page.goto("/meetings");
+  
+    console.log(await page.title());
+  
+    await expect(page.locator("body")).toBeVisible();
+  });
