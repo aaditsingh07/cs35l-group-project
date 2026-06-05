34e484aff8f747ee3c3638c3b646b70404da6b24
aaditsingh07 <aaditsingh07@gmail.com>
2026-06-04 21:58:25 -0700

    Refactor requireAuth

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,20 +1,7 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
 const db = require("../db");
+const requireAuth = require("../middleware/requireAuth");
 const router = express.Router();
-
-const JWT_SECRET = "cs35l-secret-key";
-
-function requireAuth(req, res, next) {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Not authenticated." });
-  try {
-    req.user = jwt.verify(token, JWT_SECRET);
-    next();
-  } catch {
-    res.status(401).json({ error: "Invalid or expired token." });
-  }
-}
 
 router.get("/users", requireAuth, (req, res) => {
   const userId = req.user.userId;
